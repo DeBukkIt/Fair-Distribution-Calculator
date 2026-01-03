a99a41219c90b5818f7b3f6020af010ceb2dc7c2
DeBukkIt <debukkit@googlemail.com>
2026-01-03 16:19:39 +0100

    Fixed MakeAppImage script

--- MakeAppImage.bat
+++ MakeAppImage.bat
@@ -1,14 +1,4 @@
-# 1. Minimal runtime
-jlink --module-path $JAVA_HOME/jmods \
-      --add-modules java.base,java.desktop \
-      --strip-debug --no-header-files --no-man-pages \
-      --output runtime
-
-# 2. App-Image ohne unnötige Extras
-jpackage --type app-image \
-         --name FairDistributionCalculator \
-         --input target \
-         --main-jar FairStudentDistribution-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
-         --main-class eu.ajg.fairdistribution.Main \
-         --runtime-image runtime \
-         --dest output
+@echo off
+jlink --module-path $JAVA_HOME/jmods --add-modules java.base,java.desktop --strip-debug --no-header-files --no-man-pages --output runtime
+jpackage --type app-image --name FairDistributionCalculator --input target --main-jar FairStudentDistribution-0.0.1-SNAPSHOT-jar-with-dependencies.jar --main-class eu.ajg.fairdistribution.Main --runtime-image runtime --dest output
+pause
